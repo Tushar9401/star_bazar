b5f2143b96f2bd1709fbb1cdb714f3e0943cc0c6
Akshat <frappe@LAPTOP-8N5VPNJR.localdomain>
2026-02-14 02:04:27 -0600

    POS Theme customized

--- star_bazar/hooks.py
+++ star_bazar/hooks.py
@@ -4,6 +4,7 @@ app_publisher = "Tushar Thakkar"
 app_description = "Star Bazar"
 app_email = "tusharthakkar1996@gmail.com"
 app_license = "mit"
+app_include_css = "/assets/star_bazar/css/pos_custom_final.css"
 
 # Apps
 # ------------------
