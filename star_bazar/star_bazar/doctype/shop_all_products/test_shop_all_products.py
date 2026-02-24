47d8c429c3437a1bbba2f3856430d67f12c1a3d9
ak-2811 <shahakshat28111@gmail.com>
2026-02-24 11:29:10 -0600

    Doctype Changes

---
+++ star_bazar/star_bazar/doctype/shop_all_products/test_shop_all_products.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Tushar Thakkar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestShopAllProducts(FrappeTestCase):
+	pass
