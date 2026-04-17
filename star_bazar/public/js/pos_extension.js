297eced538a627e80c6e75607454fae668404bc8
TusharTs9401 <tushar.thakkar@technovate.in>
2026-04-17 08:30:24 +0530

    Update: POS for Non Food and Generak Item

--- star_bazar/public/js/pos_extension.js
+++ star_bazar/public/js/pos_extension.js
@@ -671,6 +671,8 @@ function attach_realtime_cart_broadcast(pos) {
                     
                     new_row.qty = 1;
                     new_row.uom = item.uom;
+                    new_row.description = item.description;
+                    new_row.income_account = "Sales - SBD";
                     new_row.rate = item.rate || 0;
                     new_row.amount = item.rate || 0;
                 }
